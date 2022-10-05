ce0e56232171d6f3321655e0c1a5553e68c4418f
Nikita Kuznetsov <nkuz1509@gmail.com>
2022-10-05 18:03:50 +0300

    add some universal changes at task_2

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -11,8 +11,8 @@ Console.Clear();
 Console.Write("Введите число: ");
 int number = int.Parse(Console.ReadLine());
 double len = Math.Floor(Math.Log10(number) + 1);
-int modul_arg = 10;
-int del_arg = 10;
+double modul_arg = 10;
+double del_arg = 10;
 if(len < 3){
     Console.WriteLine("Третьей цифры нет!");
     return;
@@ -25,20 +25,8 @@ if(len == 3)
 else if (len == 4){
     modul_arg *= 10;
 }
-else if (len == 5){
-    modul_arg *= 100;
-    del_arg *= 10;   
+else if (len >= 5){
+    modul_arg *= Math.Pow(10, len-3);
+    del_arg *= Math.Pow(10, len-4);   
 }
-else if (len == 6){
-    modul_arg *= 1000;
-    del_arg *= 100;
-}
-else if (len == 7){
-    modul_arg *= 10000;
-    del_arg *= 1000;
-}
-else if (len == 8){
-    modul_arg *= 100000;
-    del_arg *= 10000;
-}
-Console.WriteLine((number % modul_arg)/del_arg);
+Console.WriteLine((number % (int)modul_arg)/(int)del_arg);

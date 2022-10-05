5acdc7bd5d3254c32875278840ac9c54adce48d3
Nikita Kuznetsov <nkuz1509@gmail.com>
2022-10-05 18:09:56 +0300

    delete return

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -10,23 +10,22 @@ Console.Clear();
 
 Console.Write("Введите число: ");
 int number = int.Parse(Console.ReadLine());
-double len = Math.Floor(Math.Log10(number) + 1);
+double len = Math.Floor(Math.Log10(number) + 1);    //Орпеделяет длину числа.
 double modul_arg = 10;
 double del_arg = 10;
 if(len < 3){
     Console.WriteLine("Третьей цифры нет!");
-    return;
 }
 if(len == 3)
 {
     Console.WriteLine(number % modul_arg);
-    return;
 }
 else if (len == 4){
     modul_arg *= 10;
+    Console.WriteLine((number % (int)modul_arg)/(int)del_arg);
 }
 else if (len >= 5){
     modul_arg *= Math.Pow(10, len-3);
-    del_arg *= Math.Pow(10, len-4);   
+    del_arg *= Math.Pow(10, len-4);
+    Console.WriteLine((number % (int)modul_arg)/(int)del_arg);   
 }
-Console.WriteLine((number % (int)modul_arg)/(int)del_arg);
